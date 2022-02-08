45212eb44b479048393f6b2bd4a468b5ffcde44a
Ana Carolina Sanches <70352645+AnaCarolinaSanches@users.noreply.github.com>
2022-02-08 19:40:44 -0300

    Update Program.cs

--- Numeros/Program.cs
+++ Numeros/Program.cs
@@ -6,7 +6,33 @@ namespace Numeros
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             Console.WriteLine("Digite um numero: ");
+            int NumeroDigitado = int.Parse(Console.ReadLine());
+            int maior = NumeroDigitado;
+            int menor = NumeroDigitado;
+            int somatorio = 0;
+            
+
+            for (int i = 1; i <= 4; i++)
+            {
+                Console.WriteLine("Digite outro numero: ");
+                NumeroDigitado = int.Parse(Console.ReadLine());
+
+                if (NumeroDigitado < menor)
+                {
+                    menor = NumeroDigitado;
+                }
+                if (NumeroDigitado > menor)
+                {
+                    maior = NumeroDigitado;
+                }
+                somatorio += NumeroDigitado; 
+            }
+            int media = somatorio / 5;
+            Console.WriteLine("meida é: " + media);
+            Console.WriteLine("\nO numero Maior Digitado foi:" + maior + "\n O numero Menor Digitado foi" + menor);
+
+            Console.ReadLine();
         }
     }
 }
